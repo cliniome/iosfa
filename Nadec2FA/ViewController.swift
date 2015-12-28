16fcdbb2ff67fd390e673199dac0cdfdd63646c7
Mohamed Ibrahim <snouto@mohameds-macbook-pro.local>
2015-12-28 17:27:14 +0300

    Initial Commit

---
+++ Nadec2FA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nadec2FA
+//
+//  Created by Mohamed Ibrahim on 3/17/1437 AH.
+//  Copyright © 1437 Innovative Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
